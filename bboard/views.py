6e51ddee439b16ca84d51b2cbae93bfa318e2e6e
wiky-avis <peta-holding@yandex.ru>
2020-11-29 14:39:55 +0300

    add form

--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,37 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Bb
+# from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
+from .models import Bb, Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+from django.urls import reverse_lazy
+
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context,request))
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context,request))
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def pet_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
